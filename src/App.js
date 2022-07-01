a9fea662488e64055f5289ede564f7eb329c250e
Anabel <villalobos1998@gmail.com>
2022-07-01 10:01:55 -0700

    finish search

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
     console.log('constructor')
   }
@@ -25,9 +26,23 @@ class App extends Component {
 
   render() {
     console.log('render')
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField)
+    })
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <input 
+        className='search-box' 
+        type="search" 
+        placeholder="search monsters"
+        onChange={(event) => {
+          const searchField = event.target.value.toLocaleLowerCase()
+          this.setState(() => {
+            return { searchField }
+          })
+        }}/>
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
             <h1>{monster.name}</h1>

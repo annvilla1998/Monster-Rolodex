ad6f5b58ae05fe7ef9c46d172087f8ff54cd2116
Anabel <villalobos1998@gmail.com>
2022-07-02 14:16:54 -0700

    converted to hooks

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,22 +3,34 @@ import './card-list.styles.css'
 import Card from '../card/card.component.jsx'
 
 
-class CardList extends Component {
-
-    render() {
-        const { monsters } = this.props;
-
-
-        return (
-            <div className="card-list">
-                {monsters.map((monster) => {
-                return (
-                    <Card monster={monster}/>
-                )
-                })}
-            </div>
-        )
-    }
+const CardList = ({ monsters }) => {
+    return (
+        <div className="card-list">
+            {monsters.map((monster) => {
+            return (
+                <Card monster={monster}/>
+            )
+            })}
+        </div>
+    )
 }
+
+// class CardList extends Component {
+
+//     render() {
+//         const { monsters } = this.props;
+
+
+//         return (
+//             <div className="card-list">
+//                 {monsters.map((monster) => {
+//                 return (
+//                     <Card monster={monster}/>
+//                 )
+//                 })}
+//             </div>
+//         )
+//     }
+// }
 
 export default CardList
